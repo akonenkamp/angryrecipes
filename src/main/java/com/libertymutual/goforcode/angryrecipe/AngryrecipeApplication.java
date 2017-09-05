259a4136430c7931ab77d7f26b89d477556236f3
Amanda Konenkamp <amanda.konenkamp@libertymutual.com>
2017-09-05 13:24:02 -0700

    first commit

---
+++ src/main/java/com/libertymutual/goforcode/angryrecipe/AngryrecipeApplication.java
@@ -0,0 +1,12 @@
+package com.libertymutual.goforcode.angryrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngryrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngryrecipeApplication.class, args);
+	}
+}

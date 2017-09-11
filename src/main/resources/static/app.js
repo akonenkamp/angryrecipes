204708992791faac443ff94834a967560f073879
Joshua Holt <joshua.holt@libertymutual.com>
2017-09-11 13:45:50 -0700

    trying to get all the updated stuff from master into my josh-front-end branch

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,73 @@
 const baseurl = 'http://localhost:8080/recipes';
 
+function createListElement(recipe) {
+	$('<li></li>')
+		.html(`
+				<a href="#" data-recipe-id="${recipe.id}">
+					${recipe.title}
+				</a>
+				
+				<form class="delete-recipe-form" method="post" action="/recipes/${recipe.id}">
+					<button>Delete</button>
+				</form>
+			`)
+		.appendTo($('#recipe-list'));		
+}
+
+$(document).on('submit', '.delete-recipe-form', function (e) {
+	e.preventDefault();
+	
+	$.ajax(this.action, { type: 'DELETE' })
+		.done(() => {
+			$(this)
+				.closest('li')
+				.remove();
+		})
+		.fail(error => console.error(error));
+});
+
+$('#create-recipe-form').on('submit', function (e) {
+	e.preventDefault();
+	
+	let payload = {
+		title: $('#title').val(),
+		description: $('#description').val(),
+		minutes: $('#minutes').val()
+	};
+	
+	let ajaxOptions = {
+		type: 'POST',
+		data: JSON.stringify(payload),
+		contentType: 'application/json'
+	};
+	
+	$.ajax(this.action, ajaxOptions)
+		.done(function (recipe) {
+			createListElement(recipe)
+		})
+		.fail(error => console.error(error));
+});
+
+$(document).on('submit', '#create-instruction-form', function (e) {
+	e.preventDefault();
+	
+	let payload = {
+			instruction: $('#instruction').val()
+	};
+	
+	let ajaxOptions = {
+			type: 'POST',
+			data: JSON.stringify(payload),
+			contentType: 'application/json'
+	};
+	
+	$.ajax(this.action, ajaxOptions)
+		.done(function (instruction) {
+			console.log(instruction);
+		})
+		.fail(error => console.error(error));
+});
+
 $(document).on('click', 'a[data-recipe-id]', function (e) {
 	e.preventDefault();
 	const recipeId = $(this).data('recipeId');
@@ -10,7 +78,10 @@ $(document).on('click', 'a[data-recipe-id]', function (e) {
 			.html(`
 					<h1>${data.title}</h1>
 					<h2>${data.description}</h2>
-					
+					<form id="create-instruction-form" method="post" action="/recipes/${data.id}/instructions">
+						<input type="text" id="instruction" name="instruction" >
+						<button>Add Instruction</button>
+					</form>
 			`);
 	});
 });
@@ -18,9 +89,7 @@ $(document).on('click', 'a[data-recipe-id]', function (e) {
 $.getJSON(baseurl, function (data) {
 	if (data.length) {
 		for (let recipe of data) {
-			$('<li></li>')
-			.html('<a href="#" data-recipe-id="' + recipe.id + '">' + recipe.title + '</a>')
-			.appendTo($('#recipe-list'));			
+			createListElement(recipe);		
 		}
 	} else {
 		$('<li></li>')
